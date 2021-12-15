3abe9f993325ac4a5c8df18a97d6fc1207919cdc
Christian Nally <sticksallison@gmail.com>
2021-12-15 13:58:20 -0800

    in class edits, as well as the source objectives.json file

--- w04d3/server.js
+++ w04d3/server.js
@@ -1,25 +1,15 @@
 const express = require('express');
-const port = 3211;
+const PORT = 3232;
 
-// create an express app
 const app = express();
-
-// use the ejs template engine
 app.set('view engine','ejs');
 
-// a place from which we can serve static files
-app.use(express.static('public'));
+app.use(express.static('public')); // serve static files from the public dir
 
-// routes are sequential. if the app finds one that matches, it will use that one.
-app.get('/',(req,res)=>{
-  res.status(200).render('index'); // send views/index.ejs as the response
+app.get('/', (req,res) => {
+  res.render('index');
 });
 
-app.get('*',(req,res)=>{
-  console.log('404 route called.');
-  res.status(404).render('404');
-  res.end();
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
-
-// have the app listen on the port
-app.listen(port,console.log(`Server is listening on port ${port}`));
